c28e64e14bdc89f07160515421f1d65cf97eff71
Benjamin Chouinard <chouinardbenjamin@gmail.com>
2021-06-11 19:34:31 -0400

    Rope physics first pass

---
+++ GMTK2021/Assets/Scripts/Rope.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+    public HingeJoint2D hook, linkPrefab;
+    Rigidbody2D previousLink;
+    public int nbLinks = 7;
+    public HingeJoint2D linkedClimber;
+
+    void Start()
+    {
+        GenerateRope();
+    }
+
+    public void GenerateRope()
+    {
+        previousLink = hook.GetComponent<Rigidbody2D>();
+
+        for (int i = 0; i < nbLinks; i++)
+        {
+            HingeJoint2D temp = Instantiate(linkPrefab, transform);
+            temp.connectedBody = previousLink;
+
+            previousLink = temp.GetComponent<Rigidbody2D>();
+        }
+
+        if(linkedClimber != null)
+        {
+            linkedClimber.connectedBody = previousLink;
+        }
+    }
+
+}

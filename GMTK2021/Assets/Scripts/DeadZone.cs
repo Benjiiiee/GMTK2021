24a0ea25959f76c41b78d69047d9b7845ff8c9b1
KolosAreik94 <62963592+KolosAreik94@users.noreply.github.com>
2021-06-13 11:13:08 -0400

    *deadzone sound +soundsplash lowersond climbers

--- GMTK2021/Assets/Scripts/DeadZone.cs
+++ GMTK2021/Assets/Scripts/DeadZone.cs
@@ -5,8 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class DeadZone : MonoBehaviour
 {
+    public AudioSource clip;
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        clip.Play();
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }

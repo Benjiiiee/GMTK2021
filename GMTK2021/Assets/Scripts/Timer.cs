ff1efe12c599a64b66ddbfba0b55295c19a3d763
Benjamin Chouinard <chouinardbenjamin@gmail.com>
2021-06-13 12:56:42 -0400

    Timer

---
+++ GMTK2021/Assets/Scripts/Timer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float time = 0;
+    public string timer;
+
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+        timer = time.ToString("F2");
+    }
+}

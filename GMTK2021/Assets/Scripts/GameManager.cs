67479a55e97b33fccc662cf32ea2607d3a9f33a1
Benjamin Chouinard <chouinardbenjamin@gmail.com>
2021-06-13 11:06:20 -0400

    Fade first pass

--- GMTK2021/Assets/Scripts/GameManager.cs
+++ GMTK2021/Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,22 +13,41 @@ public class GameManager : MonoBehaviour
     public int climbersTotal;
     public int climbersHolding = 0;
     public bool climbersLocked = true;
+    public CanvasGroup fade;
+    public bool fadeIn = false;
+    public bool fadeOut = false;
+    public float fadeSpeed = 0.1f;
 
     private void Start()
     {
         climbersTotal = climbers.Count;
+        fade.alpha = 1f;
+        FadeIn();
     }
 
     private void Update()
     {
         CheckClimbersNumber();
-        
+        if(fadeIn)
+        {
+            fade.alpha -= fadeSpeed * Time.deltaTime;
+            if (fade.alpha <= 0)
+                fadeIn = false;
+        }
+
+        if(fadeOut)
+        {
+            fade.alpha += fadeSpeed * Time.deltaTime;
+            if (fade.alpha >= 1)
+                fadeOut = false;
+        }
     }
 
     public void CheckClimbersNumber()
     {
         if (Input.GetKeyDown(KeyCode.R))
         {
+            fade.alpha = 1f;
             Scene scene = SceneManager.GetActiveScene(); SceneManager.LoadScene(scene.name);
         }
         if (climbersLocked)
@@ -49,4 +69,17 @@ public class GameManager : MonoBehaviour
     {
 
     }
+
+    public void FadeIn()
+    {
+        fadeOut = false;
+        fadeIn = true;
+    }
+
+    public void FadeOut()
+    {
+        fadeIn = false;
+        fadeOut = true;
+    }
+
 }

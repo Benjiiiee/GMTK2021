3ba72b31aa1c00ac7a0480e7ce77fe39981520b2
KolosAreik94 <62963592+KolosAreik94@users.noreply.github.com>
2021-06-12 20:39:32 -0400

    *BuildSettings +ScaleChange *plantPrefab

---
+++ GMTK2021/Assets/Scripts/ScaleChange.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleChange : MonoBehaviour
+{
+    Transform tr;
+
+    public bool canScale = false;
+    public Vector3 targetNewScale;
+    public float maxScaleDown;
+    //public ClimbersInput coll;
+
+    private void Start()
+    {
+        tr = transform;
+        targetNewScale = new Vector3(-0.1f, -0.1f, 0f);
+        maxScaleDown = 0.8f;
+    }
+
+    private void Update()
+    {
+        if (tr.localScale.x > 1)
+        {
+            tr.localScale = Vector3.one;
+        }
+        if (canScale)
+        {
+            tr.localScale += targetNewScale;
+            if (tr.localScale.x < maxScaleDown)
+            {
+                canScale = false;
+                targetNewScale = -targetNewScale;
+            }
+        }
+        else
+        {
+            if (tr.localScale.x < 1)
+            {
+                canScale = false; 
+                tr.localScale += targetNewScale;
+            }
+        }
+        
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<ClimbersInput>() != null)
+        {
+           canScale = true;
+        }
+        
+    }
+}

9056f74ba3bcb323b35e0a6ea495eee68ed28c4c
KolosAreik94 <62963592+KolosAreik94@users.noreply.github.com>
2021-06-13 11:57:04 -0400

    +SxfMenu +sounds *plantSound *GeyserMenu soudn

---
+++ GMTK2021/Assets/Scripts/SfxMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SfxMenu : MonoBehaviour
+{
+    public AudioSource mySource1;
+    public AudioSource mySource2;
+
+    public float timer = 1.5f;
+    public bool timerRunning = true;
+
+    void Update()
+    {
+        if (timerRunning) { 
+            if (timer > 0) timer -= Time.deltaTime;
+            else
+            {
+                timer = 0;
+                timerRunning = false;
+                mySource1.PlayDelayed(0.1f);
+                mySource2.Play();
+            }
+        }
+    }
+}
